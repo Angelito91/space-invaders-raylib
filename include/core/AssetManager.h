cd0bcdb1c387cbbac2e4e7493bc809195da2efa5
devLoren07 <dev.lorensantos@gmail.com>
2026-03-30 23:57:09 -0700

    feat: Add AssetManager class

--- include/core/AssetManager.h
+++ include/core/AssetManager.h
@@ -1,1 +1,25 @@
-#pragma
+#pragma once 
+#include <map>
+#include <string>
+#include "raylib.h"
+
+/**
+ * @brief Manage game assets to avoid duplicate loading
+ */
+class AssetManager{
+private:
+    std::map<std::string, Texture2D> textures; ///< Loaded textures indexed by name
+
+public:
+    /**
+     * @brief Loads a texture from disk or returns if its already loaded
+     * @param path Path to the texture file
+     * @return The Loaded Texture2D
+     */
+    Texture2D LoadTexture(const std::string& path);
+
+    /**
+     * @brief Unloads all loaded textures from memory
+     */
+    void UnloadAll();
+};
---
+++ src/AssetManager.cpp
@@ -0,0 +1,8 @@
+#include "AssetManager.h"
+
+Texture2D AssetManager::LoadTexture(const std::string &path){
+    return Texture2D();
+}
+
+void AssetManager::UnloadAll(){
+}
